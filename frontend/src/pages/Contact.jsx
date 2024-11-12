9e743432b0bd420d8f7c603a892b4666c397d31f
G-Natuba <natubaaa@gmail.com>
2024-11-12 16:35:04 -0300

    Fixing some code details

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,8 +15,9 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className=' text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@forever.com</p>
+          <p className=' text-gray-500'>Av. Brig. Faria Lima, 2179 <br /> Jardins, São Paulo - SP, 01451-000</p>
+          {/* Av. Brig. Faria Lima, 2179 - Jardins, São Paulo - SP, 01451-000 */}
+          <p className=' text-gray-500'>Tel: (11) 9 1234-5678 <br /> Email: admin@forever.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
