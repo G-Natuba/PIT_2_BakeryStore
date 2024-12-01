3d1fe045460f53d9601ca839bbfda435ff26ad9a
G-Natuba <natubaaa@gmail.com>
2024-12-01 20:36:09 -0300

    Correções pré avaliação

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link, NavLink } from 'react-router-dom'
+
 
 const Footer = () => {
   return (
     <div>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+      <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link>
+
 
         <div>
             <img src={assets.logo} className='mb-5 w-32' alt="" />
@@ -16,10 +21,19 @@ const Footer = () => {
         <div>
             <p className='text-xl font-medium mb-5'>COMPANY</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
+
+            <NavLink to='/' className='flex flex-col items-center gap-1'>
+            <li>Home</li>
+            </NavLink>
+            <NavLink to='/collection' className='flex flex-col items-center gap-1'>            
+            <li>Collection</li>
+            </NavLink>
+            <NavLink to='/about' className='flex flex-col items-center gap-1'>            
+            <li>About</li>
+            </NavLink>
+            <NavLink to='/contact' className='flex flex-col items-center gap-1'>            
+            <li>Contact</li>
+            </NavLink>
             </ul>
         </div>
 
